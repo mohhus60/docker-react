4181a06e0990c01c25f75b78d259761014209259
Your Name <you@example.com>
2019-04-16 10:09:34 +0000

    add to the new feture

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            i was changeed in the feture branch
           </a>
         </header>
       </div>
